6b0f40a8de8cabc53651b00cd4e724c7ed005759
Adam Larsh <W0871325@myscc.ca>
2025-11-14 23:47:09 -0500

    Created Basic HTML Added Routes and new components

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,18 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { App } from './app/app';
+import {provideRouter, Routes} from '@angular/router';
+import {bootstrapApplication} from '@angular/platform-browser';
+import {App} from './app/app';
+import {AboutMePage} from './app/about-me-page/about-me-page';
+import {Projects} from './app/projects/projects';
+import {ContactMe} from './app/contact-me/contact-me';
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+const routes: Routes = [
+  {path:'', redirectTo: '/home', pathMatch: 'full'},
+  {path: 'home', component: AboutMePage},
+  {path: 'projects', component: Projects},
+  {path: 'contact-me', component: ContactMe}
+]
+
+bootstrapApplication(App, {
+  providers: [
+    provideRouter(routes),]
+}).catch((err) => console.error(err));
